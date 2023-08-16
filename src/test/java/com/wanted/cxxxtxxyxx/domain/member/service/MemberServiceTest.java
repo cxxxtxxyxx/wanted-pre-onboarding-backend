3e75efb310b8ce8b642ecbcc9afb0ac4c02b65f7
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-16 21:06:33 +0900

    [#2] test: 로그인 서비스 테스트

--- src/test/java/com/wanted/cxxxtxxyxx/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/wanted/cxxxtxxyxx/domain/member/service/MemberServiceTest.java
@@ -1,6 +1,8 @@
 package com.wanted.cxxxtxxyxx.domain.member.service;
 
 import com.wanted.cxxxtxxyxx.domain.entity.Member;
+import com.wanted.cxxxtxxyxx.domain.member.dto.SignInRequestDto;
+import com.wanted.cxxxtxxyxx.domain.member.dto.SignInResponseDto;
 import com.wanted.cxxxtxxyxx.domain.member.dto.SignUpRequestDto;
 import com.wanted.cxxxtxxyxx.domain.member.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -9,11 +11,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -58,5 +63,32 @@ class MemberServiceTest {
 
         // then
         then(memberRepository).should(times(1)).findByEmail(anyString());
+    }
+
+    @DisplayName("Member 로그인 테스트")
+    @Test
+    void loginTest() {
+        // given
+        SignInRequestDto requestDto = SignInRequestDto.builder()
+                .email("qwer@naver.com")
+                .password("12345678")
+                .build();
+        Member member = Member.builder()
+                .id(1L)
+                .email("qwer@naver.com")
+                .password("********")
+                .build();
+        given(memberRepository.findByEmail(anyString())).willReturn(Optional.of(member));
+        given(tokenManager.generateAccessToken(any())).willReturn("accessToken");
+        given(passwordEncoder.matches(anyString(), anyString())).willReturn(true);
+
+        // when
+        SignInResponseDto responseDto = memberService.signIn(requestDto);
+
+        // then
+        then(memberRepository).should(times(1)).findByEmail(anyString());
+        then(tokenManager).should(times(1)).generateAccessToken(any());
+        assertThat(responseDto.getMemberId()).isEqualTo(1L);
+        assertThat(responseDto.getAccessToken()).isEqualTo("accessToken");
     }
 }
