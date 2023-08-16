51f3517a168b1073f3946c602f13164c88fc5e79
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-16 15:36:03 +0900

    [#2] feat: SignUpRequestDto 객체 구현

---
+++ src/main/java/com/wanted/cxxxtxxyxx/domain/member/dto/SignUpRequestDto.java
@@ -0,0 +1,24 @@
+package com.wanted.cxxxtxxyxx.domain.member.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor
+public class SignUpRequestDto {
+
+    @Pattern(regexp = "^.*@.*$")
+    @NotNull
+    @NotEmpty
+    private String email;
+
+    @NotNull
+    @NotEmpty
+    @Size(min = 8)
+    private String password;
+}
