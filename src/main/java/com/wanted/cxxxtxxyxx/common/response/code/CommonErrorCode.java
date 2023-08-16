decb0e1568eb3a3b55569fe8b7a558e53edc56c0
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-16 15:29:21 +0900

    [#2] chore: 공통 에러 코드명 변경
    성공시 Entity 앞글자 + S
    실패시 Entity 앞글지 + F

--- src/main/java/com/wanted/cxxxtxxyxx/common/response/code/CommonErrorCode.java
+++ src/main/java/com/wanted/cxxxtxxyxx/common/response/code/CommonErrorCode.java
@@ -7,10 +7,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CommonErrorCode implements ErrorCode{
-    INVALID_INPUT_VALUE(400, "C001", "올바르지 않은 입력값 입니다."),
-    METHOD_NOT_ALLOWED(405, "C002", "지원되지 않는 Http Method입니다."),
-    HANDLE_ACCESS_DENIED(403, "C006", "권한이 존재하지 않습니다."),
-    INTERNAL_SERVER_ERROR(500, "C999", "알 수 없는 서버 에러입니다.");
+    INVALID_INPUT_VALUE(400, "CF001", "올바르지 않은 입력값 입니다."),
+    METHOD_NOT_ALLOWED(405, "CF002", "지원되지 않는 Http Method입니다."),
+    HANDLE_ACCESS_DENIED(403, "CF006", "권한이 존재하지 않습니다."),
+    INTERNAL_SERVER_ERROR(500, "CF999", "알 수 없는 서버 에러입니다.");
 
     private final int status;
     private final String code;
