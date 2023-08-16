08c32caacf7518559ddb488c8d7f82c74193445c
cxxxtxxyxx <wmfajr1357@gmail.com>
2023-08-16 22:26:28 +0900

    [#5] test: 게시글 페이지 조회 엔드포인트 테스트

--- src/test/java/com/wanted/cxxxtxxyxx/domain/post/controller/PostControllerTest.java
+++ src/test/java/com/wanted/cxxxtxxyxx/domain/post/controller/PostControllerTest.java
@@ -1,6 +1,5 @@
 package com.wanted.cxxxtxxyxx.domain.post.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wanted.cxxxtxxyxx.domain.member.dto.MemberResponseDto;
 import com.wanted.cxxxtxxyxx.domain.post.code.PostCode;
@@ -17,17 +16,20 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -89,6 +91,52 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.data.post.title").hasJsonPath())
                 .andExpect(jsonPath("$.data.post.content").hasJsonPath());
         then(postService).should(times(1)).create(any(), any());
+    }
+
+    @DisplayName("[GET][/api/annual?page={page}&size={size}] 게시물 페이지를 조회합니다.")
+    @Test
+    @WithMockUser(username = "1")
+    public void getAllAnnualTest() throws Exception {
+        // given
+        List<ReadPostResponstDto> posts = IntStream.rangeClosed(1, 10)
+                .mapToObj(idx -> {
+                    MemberResponseDto member = MemberResponseDto.builder()
+                            .id(Long.parseLong(Integer.toString(idx)))
+                            .email(idx + "@naver.com")
+                            .build();
+                    PostResponseDto postResponseDto = PostResponseDto.builder()
+                            .id(Long.parseLong(Integer.toString(idx)))
+                            .title("테스트 제목 : " + idx)
+                            .content("테스트 내용 : " + idx)
+                            .build();
+
+                    return ReadPostResponstDto.builder()
+                            .member(member)
+                            .post(postResponseDto)
+                            .build();
+                })
+                .collect(Collectors.toList());
+
+        given(postService.getAll(anyInt(), anyInt())).willReturn(posts);
+
+        // when
+        mockMvc.perform(
+                        get("/api/v1/posts")
+                                .param("page", Integer.toString(1))
+                                .param("size", Integer.toString(10))
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.status").value(PostCode.READ_PAGE.getStatus()))
+                .andExpect(jsonPath("$.code").value(PostCode.READ_PAGE.getCode()))
+                .andExpect(jsonPath("$.data[0].member").hasJsonPath())
+                .andExpect(jsonPath("$.data[0].post").hasJsonPath())
+                .andExpect(jsonPath("$.data[9].member").hasJsonPath())
+                .andExpect(jsonPath("$.data[9].post").hasJsonPath())
+                .andExpect(jsonPath("$.message").value(PostCode.READ_PAGE.getMessage()));
+//
+        // then
+        then(postService).should().getAll(anyInt(), anyInt());
     }
 
 }
